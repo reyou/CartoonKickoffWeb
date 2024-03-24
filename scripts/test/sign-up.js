ea27a4eea43082f76ca715ebd41ec167bbcae84c
teoman shipahi <aytekoz@yahoo.com>
2024-03-23 22:53:07 -0500

    Sign up

--- scripts/test/sign-up.js
+++ scripts/test/sign-up.js
@@ -8,7 +8,9 @@ async function main() {
       .findElement(By.id('email'))
       .sendKeys('example@cartoonkickoff.com');
     await driver.findElement(By.id('password')).sendKeys('&xPkFS0a^b%J%K8P');
-    await driver.findElement(By.id('password')).sendKeys('&xPkFS0a^b%J%K8P');
+    await driver
+      .findElement(By.id('verifyPassword'))
+      .sendKeys('&xPkFS0a^b%J%K8P');
     await driver.findElement(By.css('button[type="submit"]')).click();
   } finally {
     // await driver.quit();
--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,9 +1,64 @@
+import { FormEvent, useState } from 'react';
 import PageLayout from '../PageLayout';
 
 export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [verifyPassword, setVerifyPassword] = useState('');
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // You should add form validation here.
+    // For example, check if passwords match and if the email format is correct.
+    // Then you can proceed to submit the form data.
+  };
+
   return (
     <PageLayout title='Sign up'>
-      <form></form>
+      <form onSubmit={handleSubmit}>
+        <div className='mb-3'>
+          <label htmlFor='emailInput' className='form-label'>
+            Email address
+          </label>
+          <input
+            type='email'
+            className='form-control'
+            id='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='passwordInput' className='form-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            className='form-control'
+            id='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className='mb-3'>
+          <label htmlFor='verifyPasswordInput' className='form-label'>
+            Verify Password
+          </label>
+          <input
+            type='password'
+            className='form-control'
+            id='verifyPassword'
+            value={verifyPassword}
+            onChange={(e) => setVerifyPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type='submit' className='btn btn-primary'>
+          Sign up
+        </button>
+      </form>
     </PageLayout>
   );
 }
